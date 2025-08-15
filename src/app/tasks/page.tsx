4e340eb1cf412a5468981ca48473cffe79c5ebe3
Heical Chandra <haical31@gmail.com>
2025-08-15 10:18:35 +0700

    Update page.tsx

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -6,7 +6,27 @@ import Link from "next/link";
 const CabangPage = () => {
   return (
 <DefaultLayout>
-  <TableFive />
+  {/* <TableFive /> */}
+  <section>
+    <h2>Browse Tasks</h2>
+    <p>Discover and create premade tasks that combine multiple instructions and specific knowledge</p>
+
+    <label className="input">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g
+      strokeLinejoin="round"
+      strokeLinecap="round"
+      strokeWidth="2.5"
+      fill="none"
+      stroke="currentColor"
+    >
+      <circle cx="11" cy="11" r="8"></circle>
+      <path d="m21 21-4.3-4.3"></path>
+    </g>
+  </svg>
+  <input type="search" required placeholder="Search" />
+</label>
+  </section>
 </DefaultLayout>
 
   );
